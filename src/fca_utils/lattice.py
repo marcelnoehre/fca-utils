e5fd82eb9a25e5138880dc379c2a27d6c23aaa53
marcelnoehre <marcelnoehre@web.de>
2025-12-04 10:57:06 +0100

    get intent/extent for a given concept

--- src/fca_utils/lattice.py
+++ src/fca_utils/lattice.py
@@ -15,12 +15,12 @@ def all_children(lattice: ConceptLattice, index: int) -> Set[int]:
 
     Returns
     -------
-    Set[int]
+    children : Set[int]
         A set of indices representing all children of the concept.
     '''
     visited = set()
     queue = deque([index])
-    result = set()
+    children = set()
 
     while queue:
         node = queue.popleft()
@@ -28,10 +28,10 @@ def all_children(lattice: ConceptLattice, index: int) -> Set[int]:
         for child in lattice.children(node):
             if child not in visited:
                 visited.add(child)
-                result.add(child)
+                children.add(child)
                 queue.append(child)
 
-    return result
+    return children
 
 def all_parents(lattice: ConceptLattice, index: int) -> Set[int]:
     '''
@@ -46,19 +46,73 @@ def all_parents(lattice: ConceptLattice, index: int) -> Set[int]:
 
     Returns
     -------
-    Set[int]
+    parents : Set[int]
         A set of indices representing all parents of the concept.
     '''
     visited = set()
     queue = deque([index])
-    result = set()
+    parents = set()
 
     while queue:
         node = queue.popleft()
         for parent in lattice.parents(node):
             if parent not in visited:
                 visited.add(parent)
-                result.add(parent)
+                parents.add(parent)
                 queue.append(parent)
 
-    return result
+    return parents
+
+def intent_of_concept(lattice: ConceptLattice, index: int) -> Set[int]:
+    '''
+    Get the intent of a concept in the lattice.
+
+    Parameters
+    ----------
+    lattice : ConceptLattice
+        The concept lattice.
+    index : int
+        The index of the concept.
+
+    Returns
+    -------
+    intent : Set[int]
+        A set of indices representing the intent of the concept.
+    '''
+    parents = all_parents(lattice, index)
+    intent = set()
+    for feature in lattice.get_concept_new_intent(index):
+        intent.add((index, feature))
+
+    for parent in parents:
+        for feature in lattice.get_concept_new_intent(parent):
+            intent.add((parent, feature))
+    
+    return intent
+
+def extent_of_concept(lattice: ConceptLattice, index: int) -> Set[int]:
+    '''
+    Get the extent of a concept in the lattice.
+
+    Parameters
+    ----------
+    lattice : ConceptLattice
+        The concept lattice.
+    index : int
+        The index of the concept.
+
+    Returns
+    -------
+    extent : Set[int]
+        A set of indices representing the extent of the concept.
+    '''
+    children = all_children(lattice, index)
+    extent = set()
+    for obj in lattice.get_concept_new_extent(index):
+        extent.add((index, obj))
+
+    for child in children:
+        for obj in lattice.get_concept_new_extent(child):
+            extent.add((child, obj))
+            
+    return extent
