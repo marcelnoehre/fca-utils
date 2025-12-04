0b7b5b8432b57a91469a767bd4bf22593e302c16
marcelnoehre <marcelnoehre@web.de>
2025-12-04 11:07:15 +0100

    compute minimal realizers

--- src/fca_utils/lattice.py
+++ src/fca_utils/lattice.py
@@ -1,6 +1,118 @@
-from typing import Set
+import copy
+import networkx as nx
+
 from collections import deque
+from itertools import combinations
 from fcapy.lattice import ConceptLattice
+from typing import Iterable, Tuple, List, Set
+
+def incomparability_graph(lattice: ConceptLattice) -> nx.Graph:
+    '''
+    Get the incomparability graph of a concept lattice.
+
+    Parameters
+    ----------
+    lattice : ConceptLattice
+        The concept lattice.
+    
+    Returns
+    -------
+    incomparability_graph : nx.Graph
+        The incomparability graph of the lattice.
+    '''
+    return nx.complement(nx.transitive_closure(lattice.to_networkx()).to_undirected())
+
+def linear_extensions_topological(lattice: ConceptLattice) -> Iterable[Tuple[int]]:
+    '''
+    Topological sorting of the concept lattice to get all linear extensions.
+
+    Parameters
+    ----------
+    lattice : ConceptLattice
+        The concept lattice.
+
+    Returns
+    -------
+    linear_extensions : Iterable[Tuple[int]]
+        An iterable of all linear extensions of the lattice.
+
+    Notes
+    -----
+    Only suitable for small lattices due to combinatorial explosion.
+    '''
+    return {tuple(le) for le in nx.all_topological_sorts(lattice.to_networkx())}
+
+def minimal_realizers(lattice: ConceptLattice, linear_extensions: Iterable[Tuple[int]], all: bool = True):
+    '''
+    Find all minimal realizers from a set of linear extensions.
+
+    Parameters
+    ----------
+    lattice : ConceptLattice
+        The concept lattice.
+    linear_extensions : Iterable[Tuple[int]]
+        An iterable of linear extensions of the lattice.
+    all : bool, optional
+        Whether to find all minimal realizers or stop after the first one is found. Default is True.
+
+    Returns
+    -------
+    dimension : int
+        The order dimension of the lattice (size of the minimal realizer).
+    minimal_realizers : Set[Iterable[Tuple[int]]]
+        A set of all minimal realizers found or the first one if 'all' is False.
+    '''
+    # find all pairs of incomparable concepts
+    incompareables = list(incomparability_graph(lattice).edges())
+    k = 1 if len(incompareables) == 0 else 2
+    minimal_realizers = set()
+
+    def _check_realizer(subset: Iterable[Tuple[int]]):
+        '''
+        Check if a subset of linear extensions forms a realizer.
+
+        Parameters
+        ----------
+        subset : Iterable[Tuple[int]]
+            A subset of linear extensions.
+        
+        Returns
+        -------
+        realizer : Set[Tuple[int]] | None
+            The realizer if the subset forms a realizer, None otherwise.
+        '''
+        for (x, y) in incompareables:
+            # check if all incomparables are reversed in at least one linear extension
+            fwd, bwd = False, False
+            for linear_extension in subset:
+                if linear_extension.index(x) < linear_extension.index(y):
+                    fwd = True
+                if linear_extension.index(y) < linear_extension.index(x):
+                    bwd = True
+            
+            if not fwd or not bwd:
+                return None
+            
+        return set(subset)
+    
+    while not minimal_realizers and k <= len(linear_extensions):
+        #iterate over all subsets of linear extensions of size k
+        for subset in combinations(linear_extensions, k):
+            realizer = _check_realizer(subset)
+            
+            # store valid realizer
+            if realizer:
+                minimal_realizers.add(copy.deepcopy(tuple(realizer)))
+
+                # search for all minimal realizers
+                if not all:
+                    break
+        
+        # no valid realizer found, increase number of linear extensions in subset
+        if not minimal_realizers:
+            k += 1
+
+    return k, minimal_realizers
 
 def all_children(lattice: ConceptLattice, index: int) -> Set[int]:
     '''
