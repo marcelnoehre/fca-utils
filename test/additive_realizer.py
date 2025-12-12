7c9a7e86959d7c6a74de720e2b23bfe6604dc993
marcelnoehre <marcelnoehre@web.de>
2025-12-12 12:00:32 +0100

    setup new clean sat for additive realizers

--- test/additive_realizer.py
+++ test/additive_realizer.py
@@ -10,14 +10,11 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 from src.linear_extension.linear_extension import *
 from src.linear_extension.realizer import *
 from src.visualize.dim_draw import *
+from src.additivity.additivity import *
 from src.additivity.additive_realizer import *
 
 # compute 3-dimensional realizer for the FM3
-context = decode_cxt('../data/FM3.cxt')
+context = decode_cxt('../data/N5.cxt')
 lattice = ConceptLattice.from_context(context)
 
-additive_realizer = AdditiveRealizer(lattice)
-
-# normal realizer without additivity constraints
-dim, realizer = additive_realizer.normal_realizer()
-DimDraw(lattice, realizer).plot_rotating_3d()
+AdditiveRealizer(lattice).realizer()
