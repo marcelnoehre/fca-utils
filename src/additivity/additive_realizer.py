36a7f3e5aa755b467dba424b56c56f8343665371
marcelnoehre <marcelnoehre@web.de>
2025-12-15 07:29:05 +0100

    fix construction of linear extensions

--- src/additivity/additive_realizer.py
+++ src/additivity/additive_realizer.py
@@ -3,9 +3,9 @@ import itertools
 from sage.sat.solvers.satsolver import SAT
 
 from fcapy.lattice import ConceptLattice
+from sympy import symbols, Eq, solve, sympify
 
 from src.fca_utils.lattice import *
-from src.fca_utils.parser import sage_poset_from_lattice
 
 class AdditiveRealizer:
     '''
@@ -20,46 +20,53 @@ class AdditiveRealizer:
     def __init__(self, lattice: ConceptLattice):
         # store lattice and poset
         self.lattice = lattice
-        self.P = sage_poset_from_lattice(lattice)
-        self.concepts = self.P.list()
+        self.concepts = self.lattice.to_networkx().nodes
 
         self.objects = {v for _, v in extent_of_concept(lattice, 0)}
         self.features = {v for _, v in intent_of_concept(lattice, len(self.concepts)-1)}
         self.base_vectors = self.features.union(self.objects)
         # store incomparable pairs
-        self.incomparable_pairs = self.P.incomparability_graph().edges(sort=True, labels=False)
+        self.incomparable_pairs = incomparability_graph(self.lattice)
         self.N_incomparable = len(self.incomparable_pairs)
+        self.sat_variables = {}
 
     def realizer(self):
         dim = 2
-        self._define_sat_variables(dim)
-        self._construct_linear_equations()
+        self._construct_linear_equations(dim)
         self._construct_additivity_clauses()
 
-    def _define_sat_variables(self, dim: int):
+    def _construct_linear_equations(self, dim: int):
         self.dimensions = [chr(97 + i) for i in range(dim)]
-        self.sat_variables = {
-            f'{d}_{bv}': i+1
-            for i, (d, bv) in enumerate(itertools.product(self.dimensions, self.base_vectors))
+        for d, bot in itertools.product(self.dimensions, [len(self.concepts)-1]):
+            self.sat_variables[f'{d}_{bot}'] = len(self.sat_variables)+1
+
+        self.variables = {
+            f"{d}_{v}"
+            for d in self.dimensions
+            for v in self.features | self.objects
+        } | {
+            f"{d}_{n}"
+            for d in self.dimensions
+            for n in self.concepts
         }
 
-    def _construct_linear_equations(self):
         self.equations = []
         visited = set()
-        queue = deque([node for node in self.lattice.parents(len(self.concepts)-1)])
+        queue = deque([len(self.concepts)-1])
 
         while queue:
             node = queue.popleft()
 
             # objects and missing features for the current node
             extent = [v for _, v in extent_of_concept(self.lattice, node)]
-            complement_intent = [f for f in self.features if f not in intent_of_concept(self.lattice, node)]
-            vars = extent + complement_intent
+            intent = [v for _, v in intent_of_concept(self.lattice, node)]
+            complement_intent = [f for f in self.features if f not in intent]
+            variables = extent + complement_intent
 
             # construct equations for the current node
             for d in self.dimensions:
-                self.sat_variables[f'{d}_{node}'] = len(self.sat_variables)
-                self.equations.append((tuple(f'{d}_{v}' for v in vars) if vars else tuple('0'), f'{d}_{node}'))
+                self.sat_variables[f'{d}_{node}'] = len(self.sat_variables)+1
+                self.equations.append((tuple(f'{d}_{v}' for v in variables) , f'{d}_{node}'))
 
             # add parents if not already processed or in queue
             visited.add(node)
@@ -67,8 +74,19 @@ class AdditiveRealizer:
                 if p not in queue and p not in visited:
                     queue.append(p)
 
+        self.symbols = symbols(self.variables)
+        self.sympy_equations = [
+            Eq(sympify(' + '.join(eq[0]) if eq[0] else '0'), sympify(eq[1]))
+            for eq in self.equations
+        ]
+        self.solution = solve(self.sympy_equations, self.symbols, dict=True)
+
     def _construct_additivity_clauses(self):
+        for d, bv in itertools.product(self.dimensions, self.base_vectors):
+            self.sat_variables[f'{d}_{bv}'] = len(self.sat_variables)+1
         # A -> B = ¬A v B
+        for k,v in self.solution[0].items():
+            print(f'{k} = {v}')
         self.additivity_clauses = [
             ([-self.sat_variables[v] for v in eq] + [self.sat_variables[y]])
             for eq, y in self.equations
