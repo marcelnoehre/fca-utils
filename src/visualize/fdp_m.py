8652e88eed98ee5642aa63cc6a38bc537d05e698
marcelnoehre <marcelnoehre@web.de>
2025-12-29 12:36:37 +0100

    minimize initial energy

--- src/visualize/fdp_m.py
+++ src/visualize/fdp_m.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from itertools import combinations
+from scipy.optimize import minimize
 from fcapy.context import FormalContext
 from fcapy.lattice import ConceptLattice
 
@@ -18,6 +19,8 @@ class FDP_Additive_Features():
             for m in self.attributes
         }
         self.N = len(self.attributes)
+        self._sup_inf_distance()
+        self._optimize_init_energy()
 
     def _sup_inf_distance(self):
         self.dsi_matrix = np.zeros((self.N, self.N))
@@ -33,3 +36,23 @@ class FDP_Additive_Features():
                 self.dsi_matrix[m_i][m_j] = dsi
                 self.dsi_matrix[m_j][m_i] = dsi
 
+
+    def _optimize_init_energy(self):
+        optimized = list(self.attribute_map.values())
+
+        def _energy_si(opt):
+            total_energy = 0
+            for i, j in combinations(range(self.N), 2):
+                dsi = abs(self.dsi_matrix[i, j])
+                if dsi > 0:
+                    dist = abs(opt[i] - opt[j])
+                    # (|pos(i) - pos(j)| - d_SI(i, j))^2
+                    total_energy += (dsi - dist)**2
+
+            return total_energy
+
+        res = minimize(_energy_si, optimized, method='BFGS')
+        self.vectors = {
+            m: np.array([res.x[i], -1])
+            for m, i in self.attribute_map.items()
+        }
---
+++ test/fdp_m.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import warnings
+
+# reference to src directory
+warnings.filterwarnings('ignore')
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname('src'), '..')))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+from src.fca_utils.parser import decode_cxt
+from src.visualize.fdp_m import FDP_Additive_Features
+
+context = decode_cxt('../data/standard_lattices/N5.cxt')
+FDP_Additive_Features(context)
